2be8024b768907dfe720168de7357865b654e336
Chang Xiong <cnxiong03@gmail.com>
2019-09-14 18:25:20 -0400

    create form add-song in html and the add code in app.js

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const list = document.querySelector("#song-list ul");
 const forms = document.forms;
 
-// delete books
+// delete songs
 list.addEventListener("click", (e) => {
     if (e.target.className == "delete") {
         const li = e.target.parentElement;
         li.parentNode.removeChild(li);
     }
 });
+
+//add songs
+const addForm = forms["add-song"];
+addForm.addEventListener("submit", function(e){
+    e.preventDefault();
+})
